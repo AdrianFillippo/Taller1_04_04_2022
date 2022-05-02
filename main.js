f2c029976040e2e818f9ea65d2b2c8ad7e784f20
AdrianFillippo <adrifillip2@gmail.com>
2022-05-02 17:02:32 -0500

    Solucion Ejercicio 1

---
+++ main.js
@@ -0,0 +1,29 @@
+let from = document.querySelector("#from1")
+let cajamensaje = document.querySelector(".solucion")
+let solucion = async()=>{
+    let config = {
+        method : from.method,
+        body: JSON.stringify({
+            num1 : Number(document.querySelector("#num1").value),
+            num2 : Number(document.querySelector("#num2").value)
+        })
+    };
+    let peticion = await fetch(from.action, config);
+    let data = await peticion.json();
+    cajamensaje.insertAdjacentHTML("beforeend", `<div>${data.respuesta}</div>`);
+}
+from.addEventListener("submit", (e)=>{
+    switch (e.submitter.dataset.accion) {
+        case "enviar":
+            solucion();
+            break;
+        case "limpiar":
+            cajamensaje.innerHTML = "";
+            from.reset();
+            break;
+        default:
+            cajamensaje.insertAdjacentHTML("beforeend", `<div>El data-accion='${e.submitter.dataset.accion}' no esta configurado</div>`)
+            break;
+    }
+    e.preventDefault();
+})
